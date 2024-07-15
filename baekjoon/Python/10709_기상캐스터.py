fc36629f3bbed176603358948d1d8d438bcc09e2
hyeonjeong ko <72601276+hyeonjeong-ko@users.noreply.github.com>
2024-07-16 01:08:20 +0900

    Update 10709_기상캐스터.py

--- baekjoon/Python/10709_기상캐스터.py
+++ baekjoon/Python/10709_기상캐스터.py
@@ -3,7 +3,7 @@ arr = [input() for _ in range(H)]
 v = [[0]*W for _ in range(H)]
 
 for i in range(H):
-    cnt -= 1 # 'c' 만나지 못하면 계속 -1 기록!
+    cnt = -1 # 'c' 만나지 못하면 계속 -1 기록!
     for j in range(W):
         if arr[i][j] == 'c': # 구름 만나면 0으로 초기화, 증가 시작!
             cnt = 0
