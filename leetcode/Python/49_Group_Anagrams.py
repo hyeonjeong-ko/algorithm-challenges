36b8be3502459520b799059383bf9f3e610d3fa6
hyeonjeong ko <72601276+hyeonjeong-ko@users.noreply.github.com>
2024-07-17 00:11:26 +0900

    Create 49_Group_Anagrams.py

---
+++ leetcode/Python/49_Group_Anagrams.py
@@ -0,0 +1,11 @@
+/*
+more+ defaultdict()  
+*/
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagrams = collections.defaultdict(list)
+
+        for word in strs:
+            anagrams[''.join(sorted(word))].append(word)
+        return list(anagrams.values())
