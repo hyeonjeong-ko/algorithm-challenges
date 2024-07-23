e9673f8010e02c41d9dda07e1a664cf4523822b2
hyeonjeong ko <72601276+hyeonjeong-ko@users.noreply.github.com>
2024-07-24 00:34:38 +0900

    Create 238_Product_of_Array_Except_Self.py

---
+++ leetcode/Python/238_Product_of_Array_Except_Self.py
@@ -0,0 +1,40 @@
+"""
+A Solution to Product of Array Except Self Problem
+
+Metadata:
+- Difficulty: Medium
+- Time Taken: 30 min
+
+Analysis:
+- Input:
+  - A list of integers, nums, with a length of n (n > 1).
+  - Example input: [1, 2, 3, 4]
+- Output:
+  - A list of integers where each element is the product of all other elements in the array.
+  - Example output: [24, 12, 8, 6] (product of all elements except self for each position)
+- Constraints:
+  - Do not use division.
+  - Achieve a time complexity of O(n).
+
+Implementation:
+- Data Structures:
+  - List to store intermediate products.
+- Explanation:
+  1. Left Products: Traverse the list from left to right, calculating the product of all elements to the left of each index.
+  2. Right Products: Traverse the list from right to left, updating the result by multiplying the product of elements to the right of each index.
+  3. Result Combination: The final list is the product of left and right products for each index.
+"""
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        left = []
+        p = 1
+        # 왼쪽 곱셈
+        for i in range(0, len(nums)):
+            left.append(p)
+            p = p * nums[i]
+        # 오른쪽 곱셈
+        p = 1
+        for i in range(len(nums) - 1, 0 - 1, -1):
+            left[i] = left[i] * p
+            p = p * nums[i]
+        return left
