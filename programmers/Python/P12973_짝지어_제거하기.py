7a48c49b4ecab9136933d9d649f0ec44f0c33c1d
hyeonjeong ko <72601276+hyeonjeong-ko@users.noreply.github.com>
2024-11-02 21:40:16 +0900

    Create P12973_짝지어_제거하기.py

---
+++ programmers/Python/P12973_짝지어_제거하기.py
@@ -0,0 +1,9 @@
+def solution(s):
+    st = []
+    for c in s:
+        if st and st[-1] == c: # 스택이 비지않고 현재문자와 맨위 문자가 같을때
+            st.pop()
+        else:
+            st.append(c) # 스택에 현재 문자 추가
+
+    return int(not st)
