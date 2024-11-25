da22e9784358bdd17213b40be9c34ab14be06ebb
hyeonjeong ko <72601276+hyeonjeong-ko@users.noreply.github.com>
2024-11-25 09:18:16 +0900

    Create 완주하지_못한_선수.java

---
+++ coding-test-success/ch02/완주하지_못한_선수.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        HashMap<String, Integer> m = new HashMap<>();
+        
+        for (String name : participant) { // 맵으로 참여자 저장
+            m.put(name, m.getOrDefault(name, 0) + 1);
+        }
+        
+        for (String name : completion) { // 완주자 명단 맵에서 제거
+            if (m.get(name) != 0) 
+                m.put(name, m.get(name) - 1);
+        }
+        
+        for (String name : m.keySet()) {
+            if(m.get(name) != 0)
+                return name;
+        }
+        return "";
+    }
+}
