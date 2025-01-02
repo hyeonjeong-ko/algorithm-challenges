57e959b09cc756b4e35a9edcc0ad5b128753f1f3
hyeonjeong ko <72601276+hyeonjeong-ko@users.noreply.github.com>
2025-01-02 22:39:38 +0900

    Create P77486_다단계_칫솔_판매.py

---
+++ programmers/Python/P77486_다단계_칫솔_판매.py
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
+        int[] answer = new int[enroll.length];
+        
+        // enroll : referral
+        Map<String, String> parent = new HashMap<>();
+        for (int i = 0; i < enroll.length; i++) {
+            parent.put(enroll[i], referral[i]);
+        }
+        
+        // 각 회원별 수익
+        Map<String, Integer> total = new HashMap<>();
+        for (String name : enroll)
+            total.put(name, 0);
+        
+        // 판매자별 이익 분배
+        for (int i = 0; i < seller.length; i++) {
+            int money = amount[i] * 100; // 판매 이익
+            String curName = seller[i];
+            
+            // 상위 노드로 이익 분배
+            while (money > 0 && !curName.equals("-")) {
+                int distributed = money / 10;
+                int remaining = money - distributed;
+                
+                total.put(curName, total.get(curName) + remaining);
+                money = distributed;
+                curName = parent.getOrDefault(curName, "-");
+            }
+        }
+        
+        // 결과 배열에 매핑
+        for (int i = 0; i < enroll.length; i++)
+            answer[i] = total.get(enroll[i]);
+        return answer;
+    }
+}
